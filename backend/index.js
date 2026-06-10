75a880fbd3dfffc3adeb8a1707769ac7f6779197
Vishakha3106 <gautamtashu201@gmail.com>
2026-06-10 19:32:33 +0530

    updated2

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,8 @@ app.use(cookieParser())
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:5173",
+  "http://localhost:5174",
+  "http://localhost:5175",
   "http://localhost:3000"
 ];
 
