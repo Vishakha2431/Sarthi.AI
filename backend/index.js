2dbafe1a71096d641687fd845a373adabf439812
Vishakha3106 <gautamtashu201@gmail.com>
2026-06-10 19:52:40 +0530

    updated origin

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,13 @@ app.use(cookieParser())
 //   "http://localhost:3000"
 // ];
 
-app.options("*", cors());
+app.use(cors({
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 
 const PORT=process.env.PORT || 8000
 
